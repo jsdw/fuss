ee87615c42343bf712cf5551803315930fcc16e9
James Wilson <me@unbui.lt>
2017-07-14 00:07:03 +0100

    more work on parser rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "1024"]
+
 #[macro_use] extern crate chomp;
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate pest;
--- src/parser2.rs
+++ src/parser2.rs
@@ -2,7 +2,16 @@ use pest::prelude::*;
 
 impl_rdp! {
     grammar! {
-        expression = { paren_expression | variable | if_then_else | function }
+        expression = {
+            { if_then_else | function | boolean | unit | prefix_expression | function_application_prefix | paren_expression | variable }
+            infix0 = { or }
+            infix1 = { and }
+            infix2 = { equal | not_equal | gt_or_equal | lt_or_equal | gt | lt }
+            infix3 = { plus | minus }
+            infix4 = { times | slash }
+            infix5 = {< pow }
+        }
+        prefix_expression = @{ minus ~ expression | negate ~ expression }
         paren_expression = _{ ["("] ~ expression ~ [")"] }
         variable = { ["$"] ~ variable_name }
         if_then_else = { ["if"] ~ expression ~ ["then"] ~ expression ~ ["else"] ~ expression }
@@ -10,12 +19,37 @@ impl_rdp! {
         function = { ["("] ~ function_args? ~ [")"] ~ ["=>"] ~ expression }
             function_args = _{ variable ~ ( [","] ~ variable )* }
 
-        function_application_prefix = { (variable | paren_expression) ~ ["("] ~ function_application_args? ~ [")"] }
+        function_application_prefix = { (prefix_expression | variable | paren_expression) ~ ["("] ~ function_application_args? ~ [")"] }
             function_application_args = _{ expression ~ ( [","] ~ expression )* }
 
         variable_name = _{ (['a'..'z'] | ['A'..'Z'] | ["_"] | ['0'..'9'])+ }
         boolean = { ["true"] | ["false"] }
-        number = { ["-"]? ~ (["0"] | ['1'..'9'] ~ ['0'..'9']*) }
+
+        string  = @{ ["\""] ~ string_contents ~ ["\""] }
+        string_contents = _{ (escaped_char | !(["\""] | ["\\"]) ~ any)* }
+        escaped_char  =  _{ ["\\"] ~ (["\""] | ["\\"] | ["/"] | ["b"] | ["f"] | ["n"] | ["r"] | ["t"]) }
+
+        unit = @{ number ~ number_suffix? }
+        number = @{ ["-"]? ~ (["0"] | ['1'..'9'] ~ ['0'..'9']*) ~ ( ["."] ~ ['0'..'9']+ )? }
+        number_suffix = @{ (['a'..'z']+ | ["%"])? }
+
+        whitespace = { ([" "] | ["\n"] | ["\t"] | ["\r"])+ }
+
+        minus = { ["-"] }
+        negate = { ["!"] }
+        plus = { ["+"] }
+        times = { ["*"] }
+        slash = { ["/"] }
+        pow = { ["^"] }
+        equal = { ["=="] }
+        not_equal = { ["!="] }
+        gt = { [">"] }
+        lt = { ["<"] }
+        lt_or_equal = { ["<="] }
+        gt_or_equal = { [">="] }
+        and = { ["&&"] }
+        or = { ["||"] }
+
     }
 }
 
@@ -24,25 +58,100 @@ mod test {
 
     use super::*;
 
-    #[test]
-    fn test_variable() {
+    macro_rules! parse_test {
 
-        let vars = vec![
-            "$hello",
-            "$2ello",
-            "$_hello_"
-        ];
+        // create test function:
+        ( $func:ident; $($rest:tt)+ ) => (
+            #[test]
+            fn $func() {
+                parse_test!{ __SINGLE $($rest)+ }
+            }
+        );
 
-        for var in vars {
-            let mut parser = Rdp::new(StringInput::new(var.clone()));
-            assert!(parser.variable());
-            assert!(parser.end());
-            assert_eq!(
-                parser.queue().iter().cloned().map(|i| i.rule).collect::<Vec<_>>(),
-                vec![Rule::variable],
-                "expected '{}' to be parsed as a variable", var);
-        }
+        ( __SINGLE ) => ();
 
+        // str => [var1,var2] tests:
+        ( __SINGLE $input:expr => [ $($token:ident),+ ]; $($rest:tt)* ) => (
+            {
+
+                let mut parser = Rdp::new(StringInput::new($input));
+                let mut toks = vec![];
+
+                $({
+                    assert!(parser.$token(), "could not parse {:?}", Rule::$token);
+                    toks.push(Rule::$token);
+                })+
+                assert!(parser.end(), "didn't end when expected!");
+
+                assert_eq!(
+                    parser.queue().iter().cloned().map(|i| i.rule).collect::<Vec<_>>(),
+                    toks,
+                    "tokens did not match those expected");
+
+            }
+            parse_test!{ __SINGLE $($rest)* }
+        );
+
+        // str => variable
+        (__SINGLE $input:expr => $token:ident; $($rest:tt)* ) => (
+            {
+
+                let s = $input.to_owned();
+                let mut parser = Rdp::new(StringInput::new($input));
+
+                assert!(parser.$token(), "could not parse {:?}", Rule::$token);
+                assert!(parser.end(), "didn't end when expected!");
+                assert_eq!(s.len(), parser.queue()[0].end);
+
+            }
+            parse_test!{ __SINGLE $($rest)* }
+        )
+
+    }
+
+    parse_test!{test_variable;
+        "$hello" => variable;
+        "$2ello" => variable;
+        "$_hello" => variable;
+    }
+
+    parse_test!{test_numeric;
+        "20" => number;
+        "-20" => number;
+        "0.12" => number;
+        "100.0" => number;
+        "100.0px" => unit;
+        "100.0em" => unit;
+        "0%" => unit;
+    }
+
+    parse_test!{test_if_then_else;
+        "if true then true else true" => if_then_else;
+        "if 20px then 10 else 100" => if_then_else;
+    }
+
+    parse_test!{test_strings;
+        r#""hello there""# => string;
+        r#""hello \"hello\" there""# => string;
+    }
+
+    parse_test!{test_functions;
+        "() => true" => function;
+        "($lark, $another) => $lark" => function;
+    }
+
+    parse_test!{test_operators;
+        "20 + 40" => expression;
+        "20 / 40" => expression;
+        "20 * 40" => expression;
+        "20 - 40" => expression;
+    }
+
+    parse_test!{test_func_applications;
+        "$hello(2px, true)" => expression;
+        "$hello(2px, true)" => function_application_prefix;
+        "!$hello(2px, true)" => expression;
+        "!$hello(2px, true)" => prefix_expression;
     }
 
 }
