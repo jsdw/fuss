7f5feb6139ca701be6e9f509bf74a687cbe6f30a
James Wilson <me@unbui.lt>
2017-07-15 17:27:42 +0100

    more work on new parser. issues with block parsing but almost there

--- src/parser2.rs
+++ src/parser2.rs
@@ -3,7 +3,7 @@ use pest::prelude::*;
 impl_rdp! {
     grammar! {
         expression = {
-            { if_then_else | function | boolean | unit | prefix_expression | function_application_prefix | paren_expression | variable }
+            { block | string | if_then_else | function | boolean | unit | application | paren_expression | variable_accessor }
             infix0 = { or }
             infix1 = { and }
             infix2 = { equal | not_equal | gt_or_equal | lt_or_equal | gt | lt }
@@ -11,29 +11,52 @@ impl_rdp! {
             infix4 = { times | slash }
             infix5 = {< pow }
         }
-        prefix_expression = @{ minus ~ expression | negate ~ expression }
+
+        block = { block_selector? ~ ["{"] ~ (block_assignment | block_css | block_expression | block )* ~ ["}"] }
+
+            block_expression = { (block_interpolated_expression | variable) ~ [";"] }
+            block_interpolated_expression = { ["${"] ~ expression ~ ["}"] }
+            block_assignment = { block_variable_assign ~ expression ~ [";"] }
+                block_variable_assign = @{ variable ~ [":"] }
+
+            block_css = { block_css_key ~ [":"] ~ block_css_value ~ [";"] }
+            block_selector = { ( block_interpolated_expression | block_selector_char )+ }
+                block_selector_char = _{ !(["${"] | ["{"]) ~ any }
+            block_css_key = { (block_interpolated_expression | block_css_key_char)+ }
+                block_css_key_char = _{ ['a'..'z'] | ["-"] }
+            block_css_value = { (block_interpolated_expression | variable | block_css_value_chars)+ }
+                block_css_value_chars = _{ !(block_interpolated_expression | variable | [";"]) ~ any }
+
+        application = { prefix_application | function_application }
+
+            prefix_application = @{ prefix_application_fn ~ prefix_application_args }
+            prefix_application_fn = { minus | negate }
+            prefix_application_args = !@{ paren_expression | function_application | variable_accessor }
+
+            function_application = { function_application_fn ~ ["("] ~ function_application_args ~ [")"] }
+            function_application_fn = { variable_accessor | paren_expression }
+            function_application_args = { ( expression ~ ([","] ~ expression)* )? }
+
         paren_expression = _{ ["("] ~ expression ~ [")"] }
-        variable = { ["$"] ~ variable_name }
+        variable = @{ ["$"] ~ variable_name }
+        variable_accessor = { variable ~ ( ["."] ~ variable_name )* }
         if_then_else = { ["if"] ~ expression ~ ["then"] ~ expression ~ ["else"] ~ expression }
 
         function = { ["("] ~ function_args? ~ [")"] ~ ["=>"] ~ expression }
             function_args = _{ variable ~ ( [","] ~ variable )* }
 
-        function_application_prefix = { (prefix_expression | variable | paren_expression) ~ ["("] ~ function_application_args? ~ [")"] }
-            function_application_args = _{ expression ~ ( [","] ~ expression )* }
-
         variable_name = _{ (['a'..'z'] | ['A'..'Z'] | ["_"] | ['0'..'9'])+ }
         boolean = { ["true"] | ["false"] }
 
         string  = @{ ["\""] ~ string_contents ~ ["\""] }
-        string_contents = _{ (escaped_char | !(["\""] | ["\\"]) ~ any)* }
-        escaped_char  =  _{ ["\\"] ~ (["\""] | ["\\"] | ["/"] | ["b"] | ["f"] | ["n"] | ["r"] | ["t"]) }
+            string_contents = _{ (escaped_char | !(["\""] | ["\\"]) ~ any)* }
+            escaped_char  =  _{ ["\\"] ~ (["\""] | ["\\"] | ["/"] | ["b"] | ["f"] | ["n"] | ["r"] | ["t"]) }
 
         unit = @{ number ~ number_suffix? }
-        number = @{ ["-"]? ~ (["0"] | ['1'..'9'] ~ ['0'..'9']*) ~ ( ["."] ~ ['0'..'9']+ )? }
-        number_suffix = @{ (['a'..'z']+ | ["%"])? }
+            number = @{ ["-"]? ~ (["0"] | ['1'..'9'] ~ ['0'..'9']*) ~ ( ["."] ~ ['0'..'9']+ )? }
+            number_suffix = @{ (['a'..'z']+ | ["%"])? }
 
-        whitespace = { ([" "] | ["\n"] | ["\t"] | ["\r"])+ }
+        whitespace = _{ ([" "] | ["\n"] | ["\t"] | ["\r"])+ }
 
         minus = { ["-"] }
         negate = { ["!"] }
@@ -64,47 +87,37 @@ mod test {
         ( $func:ident; $($rest:tt)+ ) => (
             #[test]
             fn $func() {
-                parse_test!{ __SINGLE $($rest)+ }
+                use std::fmt::Write;
+                let mut errors = String::new();
+                parse_test!{ __SINGLE (errors) $($rest)+ }
+                if errors.len() > 0 {
+                    assert!(false, "\n{}", errors);
+                }
             }
         );
 
-        ( __SINGLE ) => ();
-
-        // str => [var1,var2] tests:
-        ( __SINGLE $input:expr => [ $($token:ident),+ ]; $($rest:tt)* ) => (
-            {
-
-                let mut parser = Rdp::new(StringInput::new($input));
-                let mut toks = vec![];
-
-                $({
-                    assert!(parser.$token(), "could not parse {:?}", Rule::$token);
-                    toks.push(Rule::$token);
-                })+
-                assert!(parser.end(), "didn't end when expected!");
-
-                assert_eq!(
-                    parser.queue().iter().cloned().map(|i| i.rule).collect::<Vec<_>>(),
-                    toks,
-                    "tokens did not match those expected");
-
-            }
-            parse_test!{ __SINGLE $($rest)* }
-        );
+        ( __SINGLE ($errors:ident) ) => ();
 
         // str => variable
-        (__SINGLE $input:expr => $token:ident; $($rest:tt)* ) => (
+        (__SINGLE ($errors:ident) $input:expr => $token:ident; $($rest:tt)* ) => (
             {
 
                 let s = $input.to_owned();
                 let mut parser = Rdp::new(StringInput::new($input));
 
-                assert!(parser.$token(), "could not parse {:?}", Rule::$token);
-                assert!(parser.end(), "didn't end when expected!");
-                assert_eq!(s.len(), parser.queue()[0].end);
+                if !parser.$token(){
+                    writeln!(&mut $errors, "ERROR: could not parse!\n - wants: {:?}\n - input: \n{}\n - expected: {:?}", parser.expected(), s, Rule::$token);
+                }
+                else if !parser.end() {
+                    writeln!(&mut $errors, "ERROR: didn't end when expected!\n - wants: {:?}\n - input: \n{}\n - expected: {:?}\n - got: {:?}", parser.expected(), s, Rule::$token, parser.queue());
+                }
+                else {
+                    assert_eq!(s.len(), parser.queue()[0].end);
+                }
+                //println!("DEBUG: parse result: \n - input: {:?}\n - expected: {:?}\n - got: {:?}", s, Rule::$token, parser.queue())
 
             }
-            parse_test!{ __SINGLE $($rest)* }
+            parse_test!{ __SINGLE ($errors) $($rest)* }
         )
 
     }
@@ -113,6 +126,9 @@ mod test {
         "$hello" => variable;
         "$2ello" => variable;
         "$_hello" => variable;
+        "$hello" => expression;
+        "$2ello" => expression;
+        "$_hello" => expression;
     }
 
     parse_test!{test_numeric;
@@ -123,16 +139,23 @@ mod test {
         "100.0px" => unit;
         "100.0em" => unit;
         "0%" => unit;
+        "100.0px" => expression;
+        "100.0em" => expression;
+        "0%" => expression;
     }
 
     parse_test!{test_if_then_else;
-        "if true then true else true" => if_then_else;
+        "if true then  true \nelse true" => if_then_else;
         "if 20px then 10 else 100" => if_then_else;
+        "if true then true else true" => expression;
+        "if 20px then 10 else 100" => expression;
     }
 
     parse_test!{test_strings;
         r#""hello there""# => string;
         r#""hello \"hello\" there""# => string;
+        r#""hello there""# => expression;
+        r#""hello \"hello\" there""# => expression;
     }
 
     parse_test!{test_functions;
@@ -148,63 +171,77 @@ mod test {
     }
 
     parse_test!{test_func_applications;
+        "$hello(2px,true)" => expression;
         "$hello(2px, true)" => expression;
-        "$hello(2px, true)" => function_application_prefix;
+        "$hello(2px, true)" => application;
+        "!$hello" => expression;
+        "!($hello)" => expression;
+        "!$hello()" => expression;
+        "!($hello())" => expression;
+        "!$hello(2px)" => expression;
+        "!($hello(2px))" => expression;
+        "!($hello(2px, true))" => expression;
         "!$hello(2px, true)" => expression;
-        "!$hello(2px, true)" => prefix_expression;
+        "!$hello()" => application;
+        "!$hello(1px)" => application;
+        "!$hello(1px, true)" => application;
+        "!($hello)" => application;
+        "!($hello())" => application;
+        "!($hello(1px))" => application;
+        "!($hello(1px, true))" => application;
     }
 
-}
-
-//#[test]
-fn pest_calculator_test() {
-
-    impl_rdp! {
-        grammar! {
-            expression = _{
-                { ["("] ~ expression ~ [")"] | number }
-                addition       = { plus  | minus }
-                multiplication = { times | slash }
-                exponent       = {< pow }
-            }
-            number = @{ ["-"]? ~ (["0"] | ['1'..'9'] ~ ['0'..'9']*) }
-            plus   =  { ["+"] }
-            minus  =  { ["-"] }
-            times  =  { ["*"] }
-            slash  =  { ["/"] }
-            pow    =  { ["^"] }
-
-            whitespace = _{ [" "] }
-        }
-
-        process! {
-            compute(&self) -> i32 {
-                (&number: number) => number.parse::<i32>().unwrap(),
-                (&addition: addition, left: compute(), sign, right: compute()) => {
-                    println!("Adding: {} , SIGN: {:?}", addition, sign);
-                    match sign.rule {
-                        Rule::plus  => left + right,
-                        Rule::minus => left - right,
-                        _ => unreachable!()
-                    }
-                },
-                (_: multiplication, left: compute(), sign, right: compute()) => {
-                    match sign.rule {
-                        Rule::times => left * right,
-                        Rule::slash => left / right,
-                        _ => unreachable!()
-                    }
-                },
-                (_: exponent, left: compute(), _, right: compute()) => {
-                    left.pow(right as u32)
-                }
-            }
-        }
+    parse_test!{test_block_css;
+        "hello: there;" => block_css;
+        "${ $hello }: there;" => block_css;
+        "${ $hello }: the${ 2px }re;" => block_css;
     }
 
-    let mut parser = Rdp::new(StringInput::new("(3 + (9 + 3 * 4 + (3 + 1) / 2 - 4)) * 2 + (2^2^2^2)"));
+    parse_test!{test_block_assignment;
+        "$hello: 2px;" => block_assignment;
+        "$hello: ($a, $b) => $a + $b;" => block_assignment;
+    }
 
-    assert!(parser.expression());
-    assert_eq!(parser.compute(), 44 + 65536);
+    parse_test!{test_block_with_css_only;
+        "{}" => block;
+        "{ hello: there; }" => block;
+        "{ ${ $hello }: there; }" => block;
+        "{ ${ $hello }: the${ 2px }re; }" => block;
+    }
+
+    parse_test!{test_block_more;
+        "{ $hello: 1px; ${ $hello }: the${ 2px }re; }" => block;
+        ".some-selector value here::after { $hello: 1px; ${ $hello }: the${ 2px }re; }" => block;
+        ".some-selector value here::after { $hello: 1px; -hello-${ $world }: the${ 2px }re; }" => block;
+        ".some-class:not(:last-child) {}" => block;
+        ".some-class:not(:last-child) {
+            $hello: ($a, $b) => $a + $b;
+            $another: 2;
+            $lark: { $sub1: 2px; };
+            $more: $lark.sub1;
+        }" => block;
+        ".some-class:not(:last-child) {
+            ${ $hello(2px, 5px) };
+        }" => block;
+        ".some-class:not(:last-child) {
+            $hello;
+        }" => block;
+        ".some-class:not(:last-child) {
+            $hello: ($a, $b) => $a + $b;
+            $another: 2;
+            $lark: { $sub1: 2px; };
+            $more: $lark.sub1;
+
+            -moz-background-color: 1px solid blue;
+            border-radius: 10px;
+        }" => block;
+        ".some-class:not(:last-child) {
+            & .a-sub-class .more.another, .sub-clas-two { color: red; $subThing: -2px + 4px; }
+            -moz-background-color: 1px solid blue;
+            border-radius: 10px;
+        }" => block;
+        "{ $lark: {}; .stuff {} }" => block;
+        "{ .stuff {} $lark: {}; }" => block;
+    }
 
 }
