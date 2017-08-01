29f175e948cb42f6a172ec151e9ab7145149be7f
James Wilson <me@unbui.lt>
2017-08-01 23:09:50 +0100

    move main into child thread so that we can set the stack size. increase it for now to deal with huge FUSS files

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use std::convert::From;
 use std::path::PathBuf;
 use types::*;
 use std::io::{self, Write};
+use std::thread;
 
 fn main() {
     match run() {
@@ -33,34 +34,42 @@ fn main() {
 }
 
 fn run() -> io::Result<()> {
-    /// parse args from CLI based on our cli.yml,
-    /// and get matched commands:
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
 
-    // import the FUSS file, compiling and evaluating it.
-    let input = matches.value_of("input").unwrap();
-    let res = import_root( &PathBuf::from(input) );
+    let child = thread::Builder::new().stack_size(64 * 1024 * 1024).spawn(move || {
 
-    match res {
-        Err(e) => {
-            eprintln!("Error: {:?}", e);
-            Ok(())
-        },
-        Ok(Expr::NestedSimpleBlock(block)) => {
-            let css = outputter::to_css(&block);
-            let stdout = io::stdout();
-            let mut handle = stdout.lock();
+        /// parse args from CLI based on our cli.yml,
+        /// and get matched commands:
+        let yaml = load_yaml!("cli.yml");
+        let matches = App::from_yaml(yaml).get_matches();
 
-            // these could error:
-            handle.write_all(css.as_bytes())?;
-            handle.flush()
-        },
-        Ok(_) => {
-            eprintln!("Fuss file needs to evaluate to a css block");
-            Ok(())
+        // import the FUSS file, compiling and evaluating it.
+        let input = matches.value_of("input").unwrap();
+        let res = import_root( &PathBuf::from(input) );
+
+        match res {
+            Err(e) => {
+                eprintln!("Error: {:?}", e);
+                Ok(())
+            },
+            Ok(Expr::NestedSimpleBlock(block)) => {
+                let css = outputter::to_css(&block);
+                let stdout = io::stdout();
+                let mut handle = stdout.lock();
+
+                // these could error:
+                handle.write_all(css.as_bytes())?;
+                handle.flush()
+            },
+            Ok(_) => {
+                eprintln!("Fuss file needs to evaluate to a css block");
+                Ok(())
+            }
         }
-    }
+
+    }).unwrap();
+
+    child.join().unwrap()
+
 }
 
 
