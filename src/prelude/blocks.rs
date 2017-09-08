85cb154289ba0915b2e86bb29a61f00dbb64f899
James Wilson <me@unbui.lt>
2017-09-08 07:12:48 +0100

    merging only for CSS blocks. use scope of fisrt arg, first arg can't be undefined, merge css values too

--- src/prelude/blocks.rs
+++ src/prelude/blocks.rs
@@ -4,36 +4,50 @@ use std::collections::HashMap;
 /// merge variables declared in blocks, outputting a single block. Ignore Undefined variables.
 pub fn merge(args: &Vec<Expression>, _context: &Context) -> PrimRes {
 
-    let mut out = HashMap::new();
+    let mut scope = HashMap::new();
+    let mut css = Vec::new();
+    let mut selector = String::new();
+    let mut seen_first = false;
 
     for e in args {
-
         match e.expr {
-            Expr::Undefined => {},
+            Expr::Undefined => {
+                if !seen_first {
+                    return Err(ErrorType::WrongTypeOfArguments{
+                        message: "the first argument should not be undefined".to_owned()
+                    });
+                }
+            },
             Expr::EvaluatedBlock(ref b) => {
-                if let Some(scope) = b.block.scope() {
-                    for (key,val) in scope {
+                if let Block::CSSBlock(ref c) = b.block {
+                    for (key,val) in &c.scope {
                         if val.expr == Expr::Undefined { continue }
-                        out.insert(key.clone(), val.clone());
+                        scope.insert(key.clone(), val.clone());
+                    }
+                    for item in &c.css {
+                        css.push(item.clone());
+                    }
+                    if !seen_first {
+                        selector = c.selector.clone();
                     }
                 }
             },
             _ => {
                 return Err(ErrorType::WrongTypeOfArguments{
-                    message: "merge_variables only accepts block or undefined variables".to_owned()
+                    message: "only accepts css blocks or undefined variables".to_owned()
                 });
             }
         }
-
+        seen_first = true;
     }
 
     Ok(Expr::EvaluatedBlock(EvaluatedBlock{
         start: Position::new(),
         end: Position::new(),
         block: Block::CSSBlock(CSSBlock{
-            scope: out,
-            selector: String::new(),
-            css: Vec::new()
+            scope: scope,
+            selector: selector,
+            css: css
         })
     }))
 
